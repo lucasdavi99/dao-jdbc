86f3887a03fb42005bace6b9e1303899e6208394
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2023-12-31 18:32:30 -0300

    update implementation

--- src/Main.java
+++ src/Main.java
@@ -35,5 +35,11 @@ public class Main {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserido! O novo id é -> " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller  update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
     }
 }
