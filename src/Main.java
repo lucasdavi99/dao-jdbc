a734f88c6b43a0ded54a3df8dedc1a2ecc4d19b6
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2023-12-31 13:55:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -8,6 +10,8 @@ public class Main {
 
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
         System.out.println(seller);
