e16c25142392aed9fb4cce98cce28c1ff5f01827
Lucas Davi <95988791+lucasdavi99@users.noreply.github.com>
2023-12-31 13:39:44 -0300

    Seller entity class

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
-import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Books");
         System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        System.out.println(seller);
     }
 }
